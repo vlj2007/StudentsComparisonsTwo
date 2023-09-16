269cae21152e7c2a4564cbc6ac42e26828dffbc0
vlj2007 <vlj2007@yandex.ru>
2023-09-16 13:30:02 +0700

    init new method MyApp

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("2 курс, ДЗ 2");
+        MyApp();
+    }
+
+    public static void MyApp() {
+
+    }
+
+
+}
